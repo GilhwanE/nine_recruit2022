c2c36eff4ddfdd61c85a6d77f578bd956d84d4b6
GilhwanE <dyghks123@gmail.com>
2022-06-14 15:55:46 +0900

    💄  Hot_section styling

--- main.js
+++ main.js
@@ -11,11 +11,26 @@ function displayItems(items) {
 
 function createHTMLString(item) {
   return `
-        <li class="item">
-            <span>${item.title}</span>
-            <span>${item.licenseOrgan}</span>
-            <span>${item.esRegdt.slice(0, 10)}</span<
-        </li>
+      <li class="item">
+        <a href="#">
+          <div class="itemBox">
+            <img src="${item.image}" alt="${item.title}"/>
+            <h3 class="name">${item.title}</h3>
+            <h5 class="item_day">${item.Dday}</h5>
+          </div>
+
+
+          <div class="sub_txt">
+            <p>시행기관 : ${item.licenseOrgan}</p>
+            <span class="date">시행일 : ${item.esRegdt.slice(0, 10)}</span>
+          </div>
+
+          <div class="over_txt">
+            <p><strong>유형:</strong> ${item.round}</p>
+            <p><strong>접수기간:</strong> ${item.period}</p>
+          </div>
+        </a>
+      </li>
     `;
 }
 
